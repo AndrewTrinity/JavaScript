d895967a60d09baa851d67794097109b48fa6954
AndrewTrinity <andrewtrinity5000@gmail.com>
2021-03-05 13:14:12 +0100

    Added new method(reduce) and excersises, and started html

--- test.js
+++ test.js
@@ -29,13 +29,58 @@ const impares = (array) => {
 // console.log(impares(testArray))
 
 const impares2 = (array) => array.filter(e => e % 2 !== 0)
-console.log(impares2(testArray))
+// console.log(impares2(testArray))
 
 
 
 //crea una funcion que te devuelva los elementos pares del array al cuadrado
 
 const square = (array) => array.filter(e => e % 2 === 0).map(e => e * e)
-console.log(square([1, 3, 4, 5, 6, 7, 8, 9, 10]))
+// console.log(square([1, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 //Suma de elmentos del array con y sin reduce
+
+// const suma = (array) => {
+//     let acc = 0;
+//     for (let el of array) {
+//         console.log(acc, el)
+//         acc += el
+//         console.log(acc)
+//         console.log("***********************")
+//     }
+//     return acc
+// }
+
+const arrrraraa = [1, 3, 4, 5, 6, 7, 8, 9, 10]
+const suma = arrrraraa.reduce(function (acc, curr) {
+    return acc + curr;
+})
+
+const trollArray = [
+    { name: "frodo", age: 28 },
+    { name: "sam", age: 28 },
+    { name: "merry", age: 25 },
+    { name: "pippin", age: 25 },
+    { name: "gandalf", age: 130 }
+]
+
+// sumame la edad solo de los hobbits.
+// Puedes utilizar map, filter y reduce.
+
+const hobbitsAge = trollArray.map(e => e.age).filter(e => e < 130)
+const hobbitsAgeAvg = Math.floor(hobbitsAge.reduce((acc, curr) => acc + curr) / hobbitsAge.length)
+
+// console.log(hobbitsAgeAvg)
+
+
+const randNumb = (x) => Math.floor(Math.random() * x)
+console.log(randNumb())
+
+const cuentaAtras = (num) => {
+    const rand = randNumb(num)
+    console.log(rand, "random!")
+    for (let i = num; i > rand || i === 0; i--) {
+        console.log(i)
+    }
+}
+console.log(cuentaAtras(20))
